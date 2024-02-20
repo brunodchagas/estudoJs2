58b590c92221e2e97387448d2ede498b20392c19
brunodchagas <brunodchagas@yahoo.com.br>
2024-02-20 20:25:40 -0300

    commit

---
+++ exemplo2_Exception.js
@@ -0,0 +1,16 @@
+const numeros = [1,2,3,4,"A"];
+
+try {
+    const doubleNumbers = numeros.map((num)=> {
+        if(typeof num !== 'number'){
+            throw new Error ("O array só pode ter números")
+        }
+        return num *2;
+    })
+
+    console.log(doubleNumbers);
+
+}catch(error){
+    console.log("Ocorreu uma exceção: "+error.message)
+}
+
